e7b721bef8be198f6e0660cf34fbf92561026506
mgolshan <mgolshan@gmail.com>
2016-02-02 12:10:00 -0500

    Optimized and fixed bug

--- views/alert.blade.php
+++ views/alert.blade.php
@@ -1,21 +1,22 @@
+@if(session()->has('flashalert'))
 @section('styles')
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css" />
+    <link rel="stylesheet" href="/public/css/sweetalert.min.css"/>
 @endsection
 
 @section('scripts')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
-
-@if(session()->has('flashalert'))
-<script>
-swal({
-    title: "{{session('flashalert.title')}}",
-    text: "{{session('flashalert.message')}}",
-    type: "{{session('flashalert.level')}}",
-    timer: "{{config('flashalert.hide_timer')}}",
-    showConfirmButton: "{{config('flashalert.show_confirmation_button')}}"
-});
-</script>
-@endif
+    <script src="/public/js/jquery.min.js"></script>
+    <script src="/public/js/sweetalert.min.js"></script>
+    <script>
+        swal({
+            title: "{{session('flashalert.title')}}",
+            text: "{!! addslashes(session('flashalert.message')) !!}",
+            html: {{config('flashalert.html_format')}},
+            type: "{{session('flashalert.level')}}",
+            timer: "{{config('flashalert.hide_timer')}}",
+            showConfirmButton: "{{config('flashalert.show_confirmation_button')}}"
+        });
+    </script>
 
 @endsection
+<?php session()->forget('flashalert'); ?>
+@endif
